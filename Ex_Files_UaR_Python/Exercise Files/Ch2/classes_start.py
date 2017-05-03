8ec8be0868a40eb5a8fc8b3214ef031d7f3caaf2
lzuo7 <lingkai.zuo@gatech.edu>
2017-05-02 18:06:12 -0700

    update for my code

--- Ex_Files_UaR_Python/Exercise Files/Ch2/classes_start.py
+++ Ex_Files_UaR_Python/Exercise Files/Ch2/classes_start.py
@@ -1,9 +1,29 @@
 #
 # Example file for working with classes
 # (For Python 3.x, be sure to use the ExampleSnippets3.txt file)
+class MyClass():
+  def method1(self):
+    print("MyClass method1")
 
-      
+  def method2(self, someString):
+    print("MyClass method2: " + someString)
+
+class AnotherClass(MyClass):
+  def method2(self):
+    print("Another Class Method2")
+
+  def method1(self):
+    MyClass.method1(self)
+    print("AnotherClass Method1")
+
 def main():
+  c1 = MyClass()
+  c1.method1()
+  c1.method2("Lingkai Zuo")
+  print("--------------------")
+  c2 = AnotherClass()
+  c2.method2()
+  c2.method1()
   
 if __name__ == "__main__":
   main()
