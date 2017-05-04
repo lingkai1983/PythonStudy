83fd67dc60d77c49496134a91fb323df5151f054
Zuo, Lingkai <lingkai.zuo@gatech.edu>
2017-05-03 22:59:32 -0700

    update

--- Ex_Files_Programming_Realworld/Exercise Files/Ch02/02_02/start_02_02_creating_jeans.py
+++ Ex_Files_Programming_Realworld/Exercise Files/Ch02/02_02/start_02_02_creating_jeans.py
@@ -1,18 +1,23 @@
 """ The Blueprints for Jeans """
 
-class jeans:
-
-    def __init__(self, waist, length, color):
+class Jeans:
+    def __init__(self, waist=0,length=0,color='blue'):
         self.waist = waist
         self.length = length
         self.color = color
         self.wearing = False
-    
+
     def put_on(self):
-        print('Putting on {}x{} {} jeans'.format(self.waist, self.length, self.color))
+        print ("Putting on {}x{} {} jeans".format(self.waist,self.length,self.color))
         self.wearing = True
 
-
     def take_off(self):
-        print('Taking off {}x{} {} jeans'.format(self.waist, self.length, self.color))
+        print "Taking off {}x{} {} jeans".format(self.waist,self.length,self.color)
         self.wearing = False
+
+
+myJeans = Jeans(30,30,"blue");
+print dir(myJeans)
+print type(myJeans)
+myJeans.put_on()
+myJeans.take_off()
